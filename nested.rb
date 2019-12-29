0ec99dfb891142766472d841e959adcad86b100b
DAVID SHIN <kilcmn3@hotmail.com>
2019-12-29 23:18:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash = programmer_hash[:yukihiro_matsumoto] = "Hello"
+programmer_hash.store(:yukihiro_matsumoto , "Hello")
 
 puts programmer_hash
 
