07a33b7d9f9b7b1f5c18133e4daac0e52d6ac827
DAVID SHIN <kilcmn3@hotmail.com>
2019-12-29 22:59:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,10 +81,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
- programmer_hash[:yukihiro_matsumoto] =  {
+ programmer_hash.merge(:yukihiro_matsumoto =  {
     :known_for => "Ruby",
      :languages => ["LISP", "C"]
-  }
+  })
 
 
 def changing_alan
