16ab0d9e4cb139d9c72a73bd6eb26d2e57e229c8
DAVID SHIN <kilcmn3@hotmail.com>
2019-12-30 00:39:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -113,9 +113,6 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
   changing_alan =  programmer_hash[:alan_kay][:known_for] ="GUI"
 
-    puts programmer_hash
-
-
 end
 
 def adding_to_dennis
